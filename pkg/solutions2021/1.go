1dfcc3c52b7953c4bcdcaa738c6b2e5e8e005d0a
Kevin Pageau <pageau.kevin@gmail.com>
2021-12-03 22:03:20 -0500

    Complete day 1

--- pkg/solutions2021/1.go
+++ pkg/solutions2021/1.go
@@ -1,5 +1,35 @@
 package solutions2021
 
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
 func (s Solution) S1(input string) string {
-	return input
+	inputs := strings.Split(input, "\n")
+
+	increaseCount := 0
+	previousNum := -1
+
+	for _, inputLine := range inputs {
+		inputNum, err := strconv.Atoi(inputLine)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Edge case (first iteration)
+		if previousNum == -1 {
+			previousNum = inputNum
+			continue
+		}
+
+		if inputNum > previousNum {
+			increaseCount++
+		}
+
+		previousNum = inputNum
+	}
+
+	return strconv.Itoa(increaseCount)
 }
